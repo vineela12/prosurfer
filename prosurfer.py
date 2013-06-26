88a8dd6279d79c87f44884350d3cde507e42bbe9
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-06-26 19:20:39 +0530

    Twist prosurfer
    Prosurfer is now completely async and uses Twisted instead of irc.py

--- prosurfer.py
+++ prosurfer.py
@@ -2,16 +2,15 @@ import sys
 
 from importlib import import_module
 
-import irc.bot
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol
 
 from util import Struct
 from config import config
 import modules
 
-class ProSurfer(irc.bot.SingleServerIRCBot):
-  def __init__(self, config):
-
-    self.config = Struct(config) 
+class ProSurfer(irc.IRCClient):
+  def __init__(self):
     self.hooks = {
       'pubcmd'   : {},
       'pubmsg'   : [],
@@ -22,22 +21,25 @@ class ProSurfer(irc.bot.SingleServerIRCBot):
       'chanmode' : [],
     }
 
-    super(ProSurfer, self).__init__(
-        [(self.config.server, int(self.config.port))],
-        self.config.nickname, self.config.username)
+  def connectionMade(self):
+    map(self._register_module, self.factory.config.mods.split(','))
 
-    map(self._register_module, self.config.mods.split(','))
+    self.nickname = self.factory.config.nickname
+    self.channel = self.factory.config.channel
 
-    self.channel = self.config.channel
+    irc.IRCClient.connectionMade(self)
 
-  def on_welcome(self, conn, event):
-    conn.join(self.channel)
+  def connectionLost(self, reason):
+    irc.IRCClient.connectionLost(self, reason)
+
+  def signedOn(self):
+    self.join(self.channel)
 
   def on_pubmsg(self, conn, event):
     message = event.arguments[0]
     # if it's a 'command'
-    if message.startswith(self.config.prompt):
-      prompt_length = len(self.config.prompt)
+    if message.startswith(self.factory.config.prompt):
+      prompt_length = len(self.factory.config.prompt)
       command = message[prompt_length:message.find(' ', prompt_length)]
 
       self.hooks['pubcmd'][command](event.source, message[prompt_length+len(command):])
@@ -45,7 +47,7 @@ class ProSurfer(irc.bot.SingleServerIRCBot):
       map(lambda hook: hook(event), self.hooks['pubmsg'])
 
   def _register_module(self, module):
-    self.register_module(import_module('modules.' + module), self.config)
+    self.register_module(import_module('modules.' + module), self.factory.config)
 
   def register_module(self, module, config):
     md = module.Module(config)
@@ -57,5 +59,28 @@ class ProSurfer(irc.bot.SingleServerIRCBot):
     for hook in args_hooks:
       self.hooks[hook[0]][hook[1]] = getattr(md, hook[0])
 
+class ProSurferFactory(protocol.ClientFactory):
+  """
+  A factory for prosurfers.
+  A new protocol instance will be created each time we connect to the server.
+  """
+  def __init__(self, config):
+    self.config = config
+
+  def buildProtocol(self, addr):
+    prosurfer = ProSurfer()
+    prosurfer.factory = self
+    return prosurfer
+
+  def clientConnectionLost(self, connector, reason):
+    connector.connect()
+
+  def clientConnectionFailed(self, connector, reason):
+    print "connection failed:", reason
+    reactor.stop()
+
 if __name__ == "__main__":
-  ProSurfer(config).start()
+  config = Struct(config)
+  p = ProSurferFactory(config)
+  reactor.connectTCP(config.server, 6667, p)
+  reactor.run()
