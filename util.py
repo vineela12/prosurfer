d92507026d50783eb8a98808afac3f6fe68bc03b
Samuel Chase <samebchase@gmail.com>
2013-06-27 00:47:59 +0530

    Remove dead code
    Rename log file

--- util.py
+++ util.py
@@ -1,8 +1,7 @@
-# Thanks, Eli Bendersky
-# http://stackoverflow.com/questions/1305532/convert-python-dict-to-object
-
-from ConfigParser import ConfigParser
-
 class Struct:
+    """
+    Thanks, Eli Bendersky
+    http://stackoverflow.com/questions/1305532/convert-python-dict-to-object
+    """
     def __init__(self, entries):
         self.__dict__.update(entries)
